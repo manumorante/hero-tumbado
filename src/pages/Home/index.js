faade0c4ac608784a8c2aeba5f538fa531f44dad
Manu Morante <manu@estadologico.com>
2020-10-26 13:01:21 +0000

    edit home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import { Link } from 'wouter'
 export default function Home () {
   return (
     <div className='Home'>
-      <h2>Home</h2>
+      <h2>Home page</h2>
       <ul>
         <li><Link href="/search/batman" className='Link'>Search for 'batman'</Link></li>
         <li><Link href="/search/spider-man" className='Link'>Search for 'spider-man'</Link></li>
